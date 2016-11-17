5dfff673f8123a22d91e155999c3c4c676c7e9b9
pwl <bartczak94lukasz@gmail.com>
2016-11-17 18:30:57 +0100

    aaaaaaaaa

--- welcome/src/welcome/Welcome.java
+++ welcome/src/welcome/Welcome.java
@@ -3,6 +3,6 @@ public class Welcome {
 	public static void main(String[] args){
 		System.out.println("Welcome");
 		System.out.println("something");
-		System.out.println("aaaaa");
+		System.out.println("aaaaaaa");
 	}
 }
